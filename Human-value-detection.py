28cd4239bc56af7c6981e58302c4bd0f15c5be04
Francesca Ferraro <francescaferraro1707@gmail.com>
2023-12-07 19:11:53 +0100

    Human-value-detection.py

--- Human-value-detection.py
+++ Human-value-detection.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.15.2
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# +
 import pandas as pd
 import transformers
 import torch

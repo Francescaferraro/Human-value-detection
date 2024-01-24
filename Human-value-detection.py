a239e15959d77b55e0580bf1cc953d130729b9ba
Francesca Ferraro <francescaferraro1707@gmail.com>
2024-01-24 16:42:55 +0100

    Update Human-value-detection.py

--- Human-value-detection.py
+++ Human-value-detection.py
@@ -22,12 +22,16 @@ from transformers import pipeline
 import string
 from transformers import BertTokenizer, BertForMaskedLM
 import statistics
+from collections import defaultdict, Counter
+
 
 from valuemap.models import Model, MultiModel
 from valuemap.values import ValueMap, ValueSearch
 from sklearn.metrics.pairwise import cosine_similarity
 from gensim.models import KeyedVectors
-from sklearn.metrics import f1_score, accuracy_score
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+# Import the human values from values.py labels
+from valuemap.values import VALUE_LABELS
 
 
 # -
@@ -233,10 +237,6 @@ def human_value_detection(word, dictionary, model, aggregated_values):
     return normalized_values
 
 
-# Import the human values from values.py labels
-from valuemap.values import VALUE_LABELS
-
-
 def generate_word(stance, conclusion, argument, model):
     # Initialize an empty list to store the results
     results = []
@@ -321,9 +321,6 @@ mapped_adjective_values = {
 
 for adjective, values in mapped_adjective_values.items():
     print(f"{adjective}: {[VALUE_LABELS[value] for value in values]}")
-# -
-
-
 
 # +
 # Initialize an empty dictionary to store the human values for each argument ID
@@ -360,8 +357,6 @@ for argument, stance, conclusion, argument_id in zip(arguments, stances, conclus
 transformed_dict = {k: list(v.keys()) for k, v in human_values_dict.items()}
 print(transformed_dict)
 
-
-
 # +
 # Initialize an empty dictionary to store the human values for each argument ID
 human_values_dict_guided = {}
@@ -396,8 +391,6 @@ for argument, stance, conclusion, argument_id in zip(arguments, stances, conclus
 
 transformed_dict_guided = {k: list(v.keys()) for k, v in human_values_dict_guided.items()}
 print(transformed_dict_guided)
-
-
 
 # +
 human_values_dict_guidedValues = {}
@@ -436,6 +429,17 @@ print(transformed_dict_guidedValues)
 
 
 
+# +
+# For this experiment, only 10 human values are taken into consideration
+allowed_values = set(['Security', 'Conformity', 'Tradition', 'Benevolence', 'Universalism', 
+                      'Self-direction', 'Stimulation', 'Hedonism', 'Achievement', 'Power'])
+
+# Filtering the SemEval dict for avoiding the other values
+filtered_dict = {key: [value for value in values if value in allowed_values] for key, values in semeval_validation_set_results.items()}
+# -
+
+
+
 def calculate_average_metrics(predicted_dict, ground_truth_dict):
     total_f1 = 0
     total_accuracy = 0
@@ -443,10 +447,10 @@ def calculate_average_metrics(predicted_dict, ground_truth_dict):
 
     for key in ground_truth_dict.keys():
         if key in predicted_dict:
-            # Converting human values to binary format for calculation
-            human_values = list(set(ground_truth_dict[key] + predicted_dict[key]))
-            y_true_binary = [1 if value in ground_truth_dict[key] else 0 for value in human_values]
-            y_pred_binary = [1 if value in predicted_dict[key] else 0 for value in human_values]
+            # Converting human values to binary format for calculation, in a case insensitive way
+            human_values = list(set([v.lower() for v in ground_truth_dict[key] + predicted_dict[key]]))
+            y_true_binary = [1 if value in [v.lower() for v in ground_truth_dict[key]] else 0 for value in human_values]
+            y_pred_binary = [1 if value in [v.lower() for v in predicted_dict[key]] else 0 for value in human_values]
 
             # Calculating F1 Score
             f1 = f1_score(y_true_binary, y_pred_binary, average='binary')
@@ -465,43 +469,177 @@ def calculate_average_metrics(predicted_dict, ground_truth_dict):
     return avg_f1, avg_accuracy
 
 
-f1_free, accuracy_free = calculate_average_metrics(transformed_dict, semeval_validation_set_results)
-print(f"FREE APPROACH = F1 Score: {f1}, Accuracy: {accuracy}")
+f1_free, accuracy_free = calculate_average_metrics(transformed_dict, filtered_dict)
+print(f"FREE APPROACH = F1 Score: {f1_free}, Accuracy: {accuracy_free}")
 
-f1_guided, accuracy_guided = calculate_average_metrics(transformed_dict_guided, semeval_validation_set_results)
-print(f"GUIDED APPROACH - FREE MAPPING = F1 Score: {f1}, Accuracy: {accuracy}")
+f1_guided, accuracy_guided = calculate_average_metrics(transformed_dict_guided, filtered_dict)
+print(f"GUIDED APPROACH - FREE MAPPING = F1 Score: {f1_guided}, Accuracy: {accuracy_guided}")
 
-f1_guidedMapping, accuracy_guidedMapping = calculate_average_metrics(transformed_dict_guidedValues, semeval_validation_set_results)
-print(f"GUIDED APPROACH - GUIDED MAPPING = F1 Score: {f1}, Accuracy: {accuracy}")
+f1_guidedMapping, accuracy_guidedMapping = calculate_average_metrics(transformed_dict_guidedValues, filtered_dict)
+print(f"GUIDED APPROACH - GUIDED MAPPING = F1 Score: {f1_guidedMapping}, Accuracy: {accuracy_guidedMapping}")
 
 # +
-import matplotlib.pyplot as plt
-
 models = ['Free approach', 'Guided approach', 'Guided Mapping approach']
 f1_scores = [f1_free, f1_guided, f1_guidedMapping]
 accuracies = [accuracy_free, accuracy_guided, accuracy_guidedMapping]
 
-fig, ax = plt.subplots(figsize=(6, 4))
+# Getting the x locations for the groups
+ind = np.arange(len(models)) 
 
+fig, ax = plt.subplots(figsize=(6, 4))
 bar_width = 0.35
 opacity = 0.8
 
-rects1 = plt.bar(models, f1_scores, bar_width,
-alpha=opacity,
-color='b',
-label='F1 Score')
+# Create the bars for F1 scores
+rects1 = ax.bar(ind - bar_width/2, f1_scores, bar_width, alpha=opacity, color='b', label='F1 Score')
 
-rects2 = plt.bar(models, accuracies, bar_width,
-alpha=opacity,
-color='g',
-label='Accuracy')
+# Create the bars for accuracies
+rects2 = ax.bar(ind + bar_width/2, accuracies, bar_width, alpha=opacity, color='g', label='Accuracy')
 
-plt.xlabel('Models')
-plt.ylabel('Scores')
-plt.title('Scores by model')
-plt.legend()
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_xlabel('Models')
+ax.set_ylabel('Scores')
+ax.set_title('Scores by model')
+ax.set_xticks(ind)
+ax.set_xticklabels(models)
+ax.legend()
 
 plt.tight_layout()
 plt.show()
+# -
+
+
+
+
+
+# +
+# Calculating number of occurrencies for each value in the dictionaries, in a case insensitive way
+def count_predictions(dictionary):
+    all_values = [value.lower() for values in dictionary.values() for value in values]
+    return Counter(all_values)
+
+counts_semeval = count_predictions(filtered_dict)
+counts_transformed = count_predictions(transformed_dict)
+counts_guided = count_predictions(transformed_dict_guided)
+counts_guidedValues = count_predictions(transformed_dict_guidedValues)
+
+# Creating a human values list
+human_values = list(set(list(counts_semeval.keys()) + list(counts_transformed.keys()) + list(counts_guided.keys()) + list(counts_guidedValues.keys())))
+
+# Counting list
+counts = [counts_semeval, counts_transformed, counts_guided, counts_guidedValues]
+data = [[count[value] for value in human_values] for count in counts]
+# Normalization
+total_predictions = sum([sum(count.values()) for count in counts])
+data = [[count / total_predictions for count in dataset] for dataset in data]
+
+# Radial plot
+theta = np.linspace(0.0, 2 * np.pi, len(human_values), endpoint=False)
+colors = ['b', 'r', 'y', '#fa7516']
+labels = ['SemEval validation', 'Free approach', 'Guided approach', 'Guided Mapping approach']
+
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'}, figsize=(6, 4))
+
+for i in range(len(data)):
+    lines = ax.plot(theta, data[i], color=colors[i], label=labels[i])
+
+ax.set_xticks(theta)
+ax.set_xticklabels(human_values)
+ax.yaxis.grid(True)
+ax.legend(loc='upper right', bbox_to_anchor=(2, 1.1), fontsize='small')
+
+plt.show()
+
 
 # -
+
+def calculate_metrics_per_value(predicted_dict, ground_truth_dict):
+    # Initialize a dictionary to store the metrics for each human value
+    metrics = defaultdict(lambda: {'total_precision': 0, 'total_recall': 0, 'total_f1': 0, 'count': 0})
+    # Iterate over each key in the ground truth dictionary
+    for key in ground_truth_dict.keys():
+        # Check if the key is also present in the predicted dictionary
+        if key in predicted_dict:
+            # Convert human values to binary format for calculation
+            # All values are converted to lowercase to make the function case insensitive
+            human_values = list(set([v.lower() for v in ground_truth_dict[key] + predicted_dict[key]]))
+            y_true_binary = [1 if value in [v.lower() for v in ground_truth_dict[key]] else 0 for value in human_values]
+            y_pred_binary = [1 if value in [v.lower() for v in predicted_dict[key]] else 0 for value in human_values]
+            
+            # Calculate Precision, Recall, and F1 Score for each human value
+            precision = precision_score(y_true_binary, y_pred_binary, zero_division=0)
+            recall = recall_score(y_true_binary, y_pred_binary, zero_division=0)
+            f1 = f1_score(y_true_binary, y_pred_binary)
+
+            # Update the metrics for each human value
+            for i, value in enumerate(human_values):
+                if y_true_binary[i] == 1 or y_pred_binary[i] == 1:
+                    metrics[value]['total_precision'] += precision
+                    metrics[value]['total_recall'] += recall
+                    metrics[value]['total_f1'] += f1
+                    metrics[value]['count'] += 1
+    # Calculate average Precision, Recall, and F1 Score for each human value
+    for value, data in metrics.items():
+        avg_precision = data['total_precision'] / data['count'] if data['count'] > 0 else 0
+        avg_recall = data['total_recall'] / data['count'] if data['count'] > 0 else 0
+        avg_f1 = data['total_f1'] / data['count'] if data['count'] > 0 else 0
+        metrics[value] = {'Average Precision': avg_precision, 'Average Recall': avg_recall, 'Average F1': avg_f1}
+    # Sort the metrics by F1 score in descending order
+    sorted_metrics = dict(sorted(metrics.items(), key=lambda item: item[1]['Average F1'], reverse=True))
+    for value, metric in sorted_metrics.items():
+      print(f"Human Value: {value}\n Average F1 Score: {metric['Average F1']}\n Average Precision: {metric['Average Precision']}\n Average Recall: {metric['Average Recall']}\n")
+    
+    return sorted_metrics
+
+
+metrics_free = calculate_metrics_per_value(transformed_dict, filtered_dict)
+
+metrics_guided = calculate_metrics_per_value(transformed_dict_guided, filtered_dict)
+
+metrics_guidedValues = calculate_metrics_per_value(transformed_dict_guidedValues, filtered_dict)
+
+
+def plot_results_byValues(metrics) :
+    labels = list(metrics.keys())
+    precision_vals = [metrics[l]['Average Precision'] for l in labels]
+    recall_vals = [metrics[l]['Average Recall'] for l in labels]
+    f1_vals = [metrics[l]['Average F1'] for l in labels]
+    
+    x = np.arange(len(labels))  
+    width = 0.3  
+    
+    # Dictionary for mapping human values into number ids (from 1 to 10)
+    human_values_dict = {human_value: i+1 for i, human_value in enumerate(metrics.keys())}
+    # Dictionary for legend explaination
+    legend_dict = {i: human_value for human_value, i in VALUE_LABELS.items()}
+    # Substituting labels with numbers
+    labels = [int(key) for key in VALUE_LABELS.keys()]
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width, precision_vals, width, label='Precision')
+    rects2 = ax.bar(x, recall_vals, width, label='Recall')
+    rects3 = ax.bar(x + width, f1_vals, width, label='F1 Score')
+    ax.set_ylabel('Scores')
+    ax.set_title('Scores by Human Value')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend() 
+    def autolabel(rects):
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
+    fig.tight_layout()
+    plt.show()
+    print("Legend:")
+    for num, human_value in legend_dict.items():
+        print(f"{num}: {human_value}")
+
+
+plot_results_byValues(metrics_free)
+
+plot_results_byValues(metrics_guided)
+
+plot_results_byValues(metrics_guidedValues)
